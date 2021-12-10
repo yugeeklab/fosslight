7248fea547de010e777cade2bce4eb0db137162a
yugeeklab <yugeeklab@gmail.com>
2021-12-10 13:46:33 +0900

    DEV
    Signed-off-by: yugeeklab <yugeeklab@gmail.com>

--- src/test/java/oss/fosslight/controller/OssControllerTest.java
+++ src/test/java/oss/fosslight/controller/OssControllerTest.java
@@ -18,11 +18,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import oss.fosslight.domain.OssMaster;
+
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @AutoConfigureMockMvc(addFilters = false)
 @SpringBootTest
@@ -101,6 +107,54 @@ public class OssControllerTest {
                     assertThat(responseMap.get("resCd")).isEqualTo("00");
                 });
 
+    }
+
+    @Test
+    @DisplayName("oss list should be success")
+    void ossListShouldBeSuccess() throws Exception{
+        mockMvc.perform(get("/oss/list")
+                .param("type","oss")
+                .param("parameter","")
+                .param("ossName","")
+                .param("ossNameAllSearchFlag","")
+                .param("copyrights","")
+                .param("licenseName","")
+                .param("licenseNameAllSearchFlag","")
+                .param("summaryDescription", "")
+                .param("homepage","")
+                .param("deactivateFlag","")
+                .param("creator","")
+                .param("cStartDate ","")
+                .param("cEndDate","")
+                .param("modifier","")
+                .param("mStartDate","")
+                .param("mEndDate","")
+                .param("licenseType","")
+                .param("act","search")
+                .param("ossTypeSearch","")
+                .param("_search","false")
+                .param("nd","1638956903205")
+                .param("rows","15")
+                .param("page","1")
+                .param("sidx","groupKey+asc%2C+")
+                .param("sord","asc")
+                .header("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("oss autocomplete should be success")
+    void ossAutoCompleteShouldBeSuccess() throws Exception{
+        final String OSS_LIST_LENGTH = "23";
+
+        mockMvc.perform(get("/oss/autoCompleteAjax")
+                .header("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36"))
+                .andExpect(status().isOk())
+                .andDo(result -> {
+                    List<OssMaster> responseList = new ObjectMapper().readValue(result.getResponse().getContentAsString(), List.class);
+                    assertThat(responseList.size()).isEqualTo(OSS_LIST_LENGTH);
+                });
     }
 
     private void assertOssAdded(MockHttpServletResponse response) throws Exception {
